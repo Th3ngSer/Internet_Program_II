9c41ee9a52fb3e4f6d34569d935deb63d2da3243
Thongking <thongking1280@gmail.com>
2026-05-05 23:12:28 +0700

    TP04 for GrapQL

--- TP02/den-injection/src/main.ts
+++ TP02/den-injection/src/main.ts
@@ -1,7 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphqlModule } from './graphql/graphql.module';
+
+@Module({
+  imports: [
+    AppModule,
+    GraphqlModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: [join(process.cwd(), 'src/graphql/schema/*.graphql')], // shcema first 
+      // autoSchemaFile: join(process.cwd(), 'src/schema.gql'),  // code first
+
+      playground: true,
+    }),
+  ],
+})
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
