7ec6c039d498dc5d3a940fba2d5359bd5eae2afc
Thongking <thongking1280@gmail.com>
2026-05-17 11:29:47 +0700

    Commmit GraphQL more

--- TP01/todo-client/src/main.js
+++ TP01/todo-client/src/main.js
@@ -1,11 +1,17 @@
 import "./assets/main.css";
-
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { apolloClient } from './apollo/client'
 import App from "./App.vue";
-const store = createPinia();
-const app = createApp(App);
-app.use(store);
 
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+  render: () => h(App),
+})
+app.use(createPinia());
+const store = createPinia();
+app.use(store);
 app.mount("#app");
